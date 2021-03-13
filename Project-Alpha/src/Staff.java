e8d1efb98f720f0e5f49f7a8541c48b298704bee
Shreyul Patel <shp960@mail.usask.ca>
2021-03-13 16:16:20 -0600

    Completed the initial implementation for Staff class.

--- Project-Alpha/src/Staff.java
+++ Project-Alpha/src/Staff.java
@@ -1,2 +1,65 @@
 public class Staff {
+
+    private String name;
+    private String staffId;
+    private String designation;
+    private String userId;
+    private String password;
+
+    public Staff(String name, String staffId, String designation, String userId, String password)
+    {
+        this.name = name;
+        this.staffId = staffId;
+        this.designation = designation;
+        this.userId = userId;
+        this.password = password;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStaffId(){
+        return this.staffId;
+    }
+
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
+
+    public String getDesignation() {
+        return this.designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String toString()
+    {
+        return "\nName: " + this.name + "\nStaffId: " + this.staffId
+                + "\nDesignation: " + this.designation + "\n";
+    }
 }
