98786c8a83dd5544554e9aca404ca05669e50b1a
Shreyul Patel <shp960@mail.usask.ca>
2021-03-15 18:59:19 -0600

    Initial Commit....

--- Project-Alpha/src/Customer.java
+++ Project-Alpha/src/Customer.java
@@ -1,5 +1,4 @@
-public class Customer
-{
+public class Customer {
     /**
      * The name of the Customer.
      */
@@ -15,17 +14,34 @@ public class Customer
      */
     private String idProof;
 
+    /**
+     * The customer's room number
+     */
+    private int roomNumber;
 
     /**
-     * The constructor method for the customer class.
+     * Customer's Booking ID
+     */
+    private int bookingID;
+
+    /**
+     * Customer's Booking status
+     */
+    private String bookingStatus;
+
+    /**
+     *
      * @param cName Name of the customer
      * @param cNumber Contact number of the customer
-     * @param id id proof of the customer
+     * @param id Customer's proof id
      */
     public Customer(String cName, String cNumber, String id) {
         this.name = cName;
         this.contactNumber = cNumber;
         this.idProof = id;
+        this.roomNumber = -1;
+        this.bookingID = -1;
+        this.bookingStatus= null;
     }
 
     /**
@@ -82,12 +98,142 @@ public class Customer
     }
 
     /**
+     * Gets the default room number of the customer
+     * @return the room number of the customer.
+     */
+    public int getRoomNumber()
+    {
+        return this.roomNumber;
+    }
+
+    /**
+     * Sets the room number of the customer.
+     * @param roomNumber
+     */
+    public void setRoomNumber(int roomNumber)
+    {
+        this.roomNumber = roomNumber;
+    }
+
+    /**
+     * An accessor method that returns customer's booking id.
+     * @return the booking ID of the customer
+     */
+    public int getBookingID()
+    {
+        return this.bookingID;
+    }
+
+    /**
+     * An accessor method that returns customer's booking status
+     * @return booking status of the customer
+     */
+    public String getBookingStatus()
+    {
+        return this.bookingStatus;
+    }
+
+    /**
+     * Sets the customer's booking status
+     * @param bookingStatus of the customer
+     */
+    public void setBookingStatus(String bookingStatus)
+    {
+        this.bookingStatus = bookingStatus;
+    }
+
+    /**
+     * Sets the customer's booking ID
+     * @param bookingID of the customer
+     */
+    public void setBookingID(int bookingID)
+    {
+        this.bookingID = bookingID;
+    }
+
+    /**
      * Return a string representation of the customer.
      *
      * @return a string representation of the customer.
      */
     public String toString() {
         return "\nName: " + this.name + "\nContact number: " + this.contactNumber
-                + "\nID Proof: " + this.idProof + "\n";
+                + "\nID Proof: " + this.idProof + "\n"
+                +"\nRoom Number:"+ this.roomNumber
+                +"\nBooking ID:"+ this.bookingID
+                +"\nBooking Status:"+ this.bookingStatus;
+    }
+
+    public static void main(String[] args) {
+        int numErrors = 0;
+
+        Customer C1 = new Customer("Virat", "306-603-3061", "ABC456");
+
+        if (!C1.getName().equals("Virat")) {
+            System.out.println("Constructor or getName() failed: The customer's name" +
+                    " should have been Virat instead of " + C1.getName());
+            numErrors++;
+        }
+        if (!C1.getContactNumber().equals("306-603-3061")) {
+            System.out.println("Constructor or getContactNumber() failed: The customer's contact " +
+                    "number should have been 306-603-3061 instead of " + C1.getContactNumber());
+            numErrors++;
+        }
+        if (!C1.getIdProof().equals("ABC456")) {
+            System.out.println("Constructor or getIdProof() failed: The customer's proof " +
+                    "id should have been ABC456 instead of " + C1.getIdProof());
+            numErrors++;
+        }
+
+        String expected1 = "\nName: Virat\n" +
+                "Contact number: 306-603-3061\n" +
+                "ID Proof: ABC456\n";
+
+        if (!C1.toString().equals(expected1)) {
+            System.out.println("toString() failed: Expected: " + expected1 + " but returned " + C1);
+            numErrors++;
+        }
+
+        Customer C2 = new Customer("Spider Man", "999-999-9999", "SPI988");
+
+        if (!C2.getName().equals("Spider Man")) {
+            System.out.println("Constructor or getName() failed: The customer's name" +
+                    " should have been Spider Man instead of " + C1.getName());
+            numErrors++;
+        }
+        if (!C2.getContactNumber().equals("999-999-9999")) {
+            System.out.println("Constructor or getContactNumber() failed: The customer's contact " +
+                    "number should have been 999-999-9999 instead of " + C1.getContactNumber());
+            numErrors++;
+        }
+        if (!C2.getIdProof().equals("SPI988")) {
+            System.out.println("Constructor or getIdProof() failed: The customer's proof " +
+                    "id should have been SPI988 instead of " + C1.getIdProof());
+            numErrors++;
+        }
+
+        expected1 = "\nName: Spider Man\n" +
+                "Contact number: 999-999-9999\n" +
+                "ID Proof: SPI988\n";
+
+        if (!C2.toString().equals(expected1)) {
+            System.out.println("toString() failed: Expected: " + expected1 + " but returned " + C2);
+            numErrors++;
+        }
+
+        C2.setName("Vision");
+        C2.setContactNumber("111-111-1111");
+        C2.setIdProof("VIS384");
+
+        expected1 = "\nName: Vision\n" +
+                "Contact number: 111-111-1111\n" +
+                "ID Proof: VIS384\n";
+
+        if (!C2.toString().equals(expected1)) {
+            System.out.println("toString() failed: Expected: " + expected1 + " but returned " + C2);
+            numErrors++;
+        }
+
+        System.out.println("The number of errors found is " + numErrors);
     }
 }
--- Project-Alpha/src/Staff.java
+++ Project-Alpha/src/Staff.java
@@ -15,8 +15,6 @@ public class Staff {
         this.password = password;
     }
 
-
-
     public String getName() {
         return name;
     }
@@ -61,5 +59,98 @@ public class Staff {
     {
         return "\nName: " + this.name + "\nStaffId: " + this.staffId
                 + "\nDesignation: " + this.designation + "\n";
+    }
+
+    public static void main(String[] args) {
+        int numErrors = 0;
+
+        Staff S1 = new Staff("Fiza Patel", "123", "Receptionist", "FIP428", "Bhangi12");
+
+        if (!S1.getName().equals("Fiza Patel")) {
+            System.out.println("Constructor or getName() failed: The staff's name " +
+                    " should have been Fiza Patel instead of " + S1.getName());
+        }
+        if (!S1.getStaffId().equals("123")) {
+            System.out.println("Constructor or getStaffId() failed: The staff's id " +
+                    "number should have been 123 instead of " + S1.getStaffId());
+            numErrors++;
+        }
+        if (!S1.getDesignation().equals("Receptionist")) {
+            System.out.println("Constructor or getDesignation() failed: The staff's designation " +
+                    "should have been Receptionist instead of " + S1.getDesignation());
+            numErrors++;
+        }
+        if (!S1.getUserId().equals("FIP428")) {
+            System.out.println("Constructor or getUserId() failed: The staff's user id " +
+                    "number should have been FIP428 instead of " + S1.getUserId());
+            numErrors++;
+        }
+        if (!S1.getPassword().equals("Bhangi12")) {
+            System.out.println("Constructor or getPassword() failed: The staff's password " +
+                    "should have been Bhangi12 instead of " + S1.getDesignation());
+            numErrors++;
+        }
+
+        String expected1 = "\nName: Fiza Patel\n" +
+                "StaffId: 123\n" +
+                "Designation: Receptionist\n";
+
+        if (!S1.toString().equals(expected1)) {
+            System.out.println("toString() failed: Expected: " + expected1 + " but returned " + S1);
+            numErrors++;
+        }
+
+        Staff S2 = new Staff("Tony Stark", "111", "Manager", "TON123", "IronMan1");
+
+        if (!S2.getName().equals("Tony Stark")) {
+            System.out.println("Constructor or getName() failed: The staff's name " +
+                    " should have been Tony Stark instead of " + S2.getName());
+        }
+        if (!S2.getStaffId().equals("111")) {
+            System.out.println("Constructor or getStaffId() failed: The staff's id " +
+                    "number should have been 111 instead of " + S1.getStaffId());
+            numErrors++;
+        }
+        if (!S2.getDesignation().equals("Manager")) {
+            System.out.println("Constructor or getDesignation() failed: The staff's designation " +
+                    "should have been Manager instead of " + S1.getDesignation());
+            numErrors++;
+        }
+        if (!S2.getUserId().equals("TON123")) {
+            System.out.println("Constructor or getUserId() failed: The staff's user id " +
+                    "number should have been TON123 instead of " + S1.getUserId());
+            numErrors++;
+        }
+        if (!S2.getPassword().equals("IronMan1")) {
+            System.out.println("Constructor or getPassword() failed: The staff's password " +
+                    "should have been IronMan1 instead of " + S1.getDesignation());
+            numErrors++;
+        }
+
+        String expected2 = "\nName: Tony Stark\n" +
+                "StaffId: 111\n" +
+                "Designation: Manager\n";
+
+        if (!S2.toString().equals(expected2)) {
+            System.out.println("toString() failed: Expected: " + expected2 + " but returned " + S2);
+            numErrors++;
+        }
+
+        S1.setName("Hardik");
+        S1.setStaffId("254");
+        S1.setDesignation("Receptionist");
+        S1.setUserId("HAR901");
+        S1.setPassword("Power123");
+
+        expected1 = "\nName: Hardik\n" +
+                "StaffId: 254\n" +
+                "Designation: Receptionist\n";
+
+        if (!S1.toString().equals(expected1)) {
+            System.out.println("toString() failed: Expected: " + expected1 + " but returned " + S1);
+            numErrors++;
+        }
+
+        System.out.println("The number of errors found is " + numErrors);
     }
 }
